1d94d484da1ead56ada95727b5772e2cf08b2803
osram21 <osram21@naver.com>
2017-03-06 11:15:34 +0900

    added.gitignore

--- Hello.java
+++ Hello.java
@@ -1,5 +1,7 @@
 public class Hello{
 	public static void main(String[] args){
 		System.out.println("Hello World");
+		System.out.println("Tell your World");
+		System.out.println("Tell her World");
 	}
 }
